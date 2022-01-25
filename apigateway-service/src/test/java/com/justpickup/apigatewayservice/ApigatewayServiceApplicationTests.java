62a3442d446da9ee19b90ddfba9443ddbe6ff40e
hoon7566 <hoon7566@gmail.com>
2022-01-25 16:58:23 +0900

    add apigateway-service

---
+++ apigateway-service/src/test/java/com/justpickup/apigatewayservice/ApigatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justpickup.apigatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
