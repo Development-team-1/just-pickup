6ed5cb184b29cddb951366a698b4403b70dae75b
hoon7566 <hoon7566@gmail.com>
2022-01-25 20:42:46 +0900

    Create store-service project and settings

---
+++ store-service/src/test/java/com/justpickup/storeservice/StoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.justpickup.storeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
