2ef6e27a7eb8e931b1341dc1407f41f04d0d5a0a
bum12ark <bum12ark@gmail.com>
2022-02-08 18:29:08 +0900

    feat(owner-frontend, order): 점주 서비스 - 주문 페이지 검색 및 더보기 기능 구현
    - 검색 버튼 또는 더보기 버튼을 통해 주문 카드를 가져오는 로직 구현

--- order-service/src/main/java/com/justpickup/orderservice/domain/order/dto/OrderSearchCondition.java
+++ order-service/src/main/java/com/justpickup/orderservice/domain/order/dto/OrderSearchCondition.java
@@ -2,6 +2,7 @@ package com.justpickup.orderservice.domain.order.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Pattern;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +15,8 @@ public class OrderSearchCondition {
     @Pattern(regexp = "^(19|20)\\d{2}-(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[0-1])$",
             message = "YYYY-MM-DD 형식에 맞게 작성되지 않았습니다.")
     private String orderDate;
+    @Min(value = 1,
+            message = "마지막 주문 번호는 1보다 작을 수 없습니다.")
     private Long lastOrderId;
 
     public LocalDateTime getOrderStartTime() {
