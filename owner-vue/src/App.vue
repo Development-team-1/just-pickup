3da1bbb975151df950768c80c3a5a7fe432d412a
bum12ark <bum12ark@gmail.com>
2022-02-22 14:36:49 +0900

    feat(owner-vue): 점주용 vue 프로젝트 추가

---
+++ owner-vue/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app id="inspire">
+    <Sidebar :drawer="drawer" />
+    <Topbar @drawerEvent="drawer = !drawer" />
+    <v-main style="background: #f5f5f540">
+      <v-container class="py-8 px-6" fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Sidebar from "./components/Sidebar";
+import Topbar from "./components/Topbar";
+export default {
+  name: "App",
+  components: { Topbar, Sidebar },
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+    drawer: null,
+  }),
+  methods: {},
+};
+</script>
---
+++ owner-vue/src/components/Sidebar.vue
@@ -0,0 +1,52 @@
+<template>
+  <!-- <v-navigation-drawer v-model="drawer" app> -->
+  <v-navigation-drawer app>
+    <v-img
+      height="140"
+      class="pa-4"
+      src="https://preview.pixlr.com/images/800wm/1439/2/1439104804.jpg"
+    >
+      <div class="text-center">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <v-img
+            aspect-ratio="30"
+            src="https://yt3.ggpht.com/esazPAO03T0f0vKdByJvkDy6MSwjyG5f-c_2S2CJapszQ3KPQyZarpoqvgv0Us0atUbILytj=s88-c-k-c0x00ffffff-no-rj"
+          />
+        </v-avatar>
+        <h2 class="white--text">Web Burden</h2>
+      </div>
+    </v-img>
+    <v-divider></v-divider>
+    <v-list>
+      <v-list-item v-for="[icon, text] in links" :key="icon" link>
+        <v-list-item-icon>
+          <v-icon>{{ icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ text }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  props: ["drawer"],
+  data() {
+    return {
+      links: [
+        ["mdi-microsoft-windows", "Dashboard"],
+        ["mdi-account", "Profile"],
+        ["mdi-clipboard-list-outline", "Products"],
+        ["mdi-card-account-details-outline", "Orders"],
+        ["mdi-cog", "System Setting"],
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped></style>
