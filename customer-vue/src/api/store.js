af9f86ff5ca7fd00cbe787bad58d96de22bf3cd2
bum12ark <bum12ark@gmail.com>
2022-03-08 20:50:49 +0900

    feat(customer-vue): 고객 매장 상품 페이지 개발

--- customer-vue/src/api/store.js
+++ customer-vue/src/api/store.js
@@ -1,56 +1,39 @@
 import axios from "axios";
 
 export default {
-    requestNearbyStore(latitude, longitude, storeName, page, size) {
-      const options = {
-        params: {
-          latitude: latitude,
-          longitude: longitude,
-          storeName: storeName,
-          page: page,
-          size: size
-        }
+  requestNearbyStore(latitude, longitude, storeName, page, size) {
+    const options = {
+      params: {
+        latitude: latitude,
+        longitude: longitude,
+        storeName: storeName,
+        page: page,
+        size: size
       }
-      return axios.get(process.env.VUE_APP_STORE_API_URL + '/store/search', options);
-    },
-    getCategoryList(){
-        return axios.get(process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/category/');
-    },
-    putCategoryList(data){
-        return axios({
-            method:'put',
-            url:process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/category',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-            data: data,
-            responseType:'json'
-        })
-    },
-    getItemById(itemId){
-        return axios.get(process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/item/'+itemId)
-    },
-    saveItem(method, itemData){
-        return axios({
-            method:method,
-            url: process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/item',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json;charset=UTF-8'
-            },
-            data: itemData,
-            responseType:'json'
-        })
-    },
-    getFavoriteStore(latitude, longitude,){
-        const options = {
-            params: {
-                latitude: latitude,
-                longitude: longitude,
-            }
-        }
-        return axios.get(process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/api/customer/store/favorite',options)
-    },
-
+    }
+    return axios.get(process.env.VUE_APP_STORE_API_URL + '/store/search', options);
+  },
+  getItemById(itemId){
+      return axios.get(process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/item/'+itemId)
+  },
+  getFavoriteStore(latitude, longitude,){
+      const options = {
+          params: {
+              latitude: latitude,
+              longitude: longitude,
+          }
+      }
+      return axios.get(process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL+'/store-service/api/customer/store/favorite',options)
+  },
+  requestCategoriesWithItem(storeId) {
+    const options = {
+      params: {
+        "storeId": storeId
+      }
+    }
+    return axios.get(process.env.VUE_APP_STORE_API_URL + "/categories", options);
+  },
+  requestStore(storeId) {
+    return axios.get(process.env.VUE_APP_CUSTOMER_SERVICE_BASEURL + "/store-service/store/" + storeId);
+  }
 }
---
+++ customer-vue/src/views/StoreView.vue
@@ -0,0 +1,111 @@
+<template>
+  <div>
+    <div id="nav">
+      <v-chip-group
+          mandatory
+          center-active
+          show-arrows
+          active-class="primary--text"
+          v-model="tagIndex"
+      >
+        <v-chip
+            v-for="tag in tags"
+            :key="tag"
+        >
+          {{ tag }}
+        </v-chip>
+      </v-chip-group>
+    </div>
+
+    <br>
+    <div id="content">
+      <div v-for="category in categories" :key="category.id">
+        <h3 ref="focusTag">
+          {{ category.name }}
+        </h3>
+        <br>
+        <v-card
+            class="mx-auto mb-5"
+            outlined
+            v-for="item in category.items"
+            :key="item.id"
+        >
+          <v-list-item three-line>
+            <v-list-item-content>
+              <v-list-item-title class="text-h5 mb-3">
+                {{ item.name }}
+              </v-list-item-title>
+              <div class="text--primary mb-5">
+                {{ item.price }}원
+              </div>
+              <v-list-item-subtitle>
+              </v-list-item-subtitle>
+            </v-list-item-content>
+
+            <v-list-item-avatar
+                tile
+                size="100"
+            >
+              <v-img :src="require('@/assets/store.jpeg')"></v-img>
+            </v-list-item-avatar>
+          </v-list-item>
+        </v-card>
+        <br>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import storeApi from "../api/store";
+
+export default {
+  name: "StoreView",
+  props: ["storeId"],
+  mounted() {
+    if (!this.storeId) {
+      alert("잘못된 요청입니다.");
+      return;
+    }
+    this.$emit('getStoreId', this.storeId)
+    this.search(this.storeId);
+  },
+  data: () => ({
+    tags: [],
+    categories: [],
+    tagIndex: 0
+  }),
+  watch: {
+    tagIndex: function(newValue) {
+      this.$refs.focusTag[newValue].scrollIntoView({behavior: 'smooth', block: 'center'});
+    }
+  },
+  methods: {
+    search: async function(storeId) {
+      const response = await storeApi.requestCategoriesWithItem(storeId);
+      this.render(response.data);
+    },
+    render: function(json) {
+      this.categories = json.data.categories;
+      this.categories.forEach(category => {
+        this.tags.push(category.name);
+      })
+    },
+  }
+}
+</script>
+
+<style scoped>
+#nav {
+  color: white;
+  background-color: white;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+}
+
+#content {
+  display: block !important;
+  z-index: -1;
+}
+</style>
