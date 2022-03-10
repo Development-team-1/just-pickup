34f2c42887cbcccacdc8a223dd20f848ec1ddda4
bum12ark <bum12ark@gmail.com>
2022-03-10 16:45:21 +0900

    feat(owner-vue): 상태변경 로직 추가

--- owner-vue/src/components/OrderCard.vue
+++ owner-vue/src/components/OrderCard.vue
@@ -9,19 +9,44 @@
     <v-card-subtitle></v-card-subtitle>
     <v-card-text>{{ orderTime }}</v-card-text>
     <v-card-actions>
-      <v-btn v-if="orderStatus === 'PENDING'"
-          block depressed color="primary">
-        주문 대기
-      </v-btn>
-      <v-btn v-else-if="orderStatus === 'PLACED'"
-             block depressed color="primary">
-        주문 수령
-      </v-btn>
+      <v-row v-if="orderStatus === 'ORDER'">
+        <v-col sm="6">
+          <v-btn
+              block depressed color="success"
+              @click="placed"
+          >
+            주문 수령
+          </v-btn>
+        </v-col>
+        <v-col sm="6">
+          <v-btn block depressed color="error"
+            @click="reject"
+          >
+            주문 거절
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row v-else-if="orderStatus === 'PLACED'">
+        <v-col>
+          <v-btn block depressed color="primary">
+            수락됨
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row v-else-if="orderStatus === 'REJECT'">
+        <v-col>
+          <v-btn block depressed color="blue-grey" class="white--text">
+            거절됨
+          </v-btn>
+        </v-col>
+      </v-row>
     </v-card-actions>
   </v-card>
 </template>
 
 <script>
+import orderApi from "../api/order";
+
 export default {
   name: "OrderCard",
   data: function() {
@@ -30,11 +55,32 @@ export default {
     };
   },
   props: {
-    orderId: Number,
+    id: Number,
     userName: Number,
     itemNames: [],
     orderTime: String,
     orderStatus: String
+  },
+  methods: {
+    placed: async function() {
+      try {
+        await orderApi.patchOrder(this.id, 'PLACED');
+        this.$emit("placed");
+        alert("해당 주문이 수락 되었습니다.");
+      } catch(error) {
+        console.log(error);
+      }
+    },
+    reject: async function() {
+      try {
+        await orderApi.patchOrder(this.id, 'REJECT');
+        this.$emit("reject");
+        alert("해당 주문이 거절 되었습니다.");
+
+      } catch(error) {
+        console.log(error);
+      }
+    }
   }
 }
 </script>
