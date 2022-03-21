7cacc897847728775e3841d8fa9067184c90253a
hoon7566 <hoon7566@gmail.com>
2022-03-21 20:57:13 +0900

    feat(owner-vue): userInfo data 상위 컴포넌트로 이동
    - userInfo data 상위 컴포넌트로 이동

--- owner-vue/src/views/Layout/DashboardLayout.vue
+++ owner-vue/src/views/Layout/DashboardLayout.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app id="inspire">
-    <side-bar :drawer="drawer" @drawerEvent="drawer = !drawer"></side-bar>
+    <side-bar :drawer="drawer" @drawerEvent="drawer = !drawer" :userInfo="userInfo"/>
     <top-bar @drawerEvent="drawer = !drawer"
-              :notificationCounts="notificationCounts"/>
+             :notificationCounts="notificationCounts"
+             :userInfo="userInfo"
+    />
     <v-main style="background: #f5f5f540">
       <v-container class="py-8, px-6" fluid>
         <router-view
             v-on:plusCount="notificationCounts++"
             v-on:minusCount="notificationCounts--"
+            :userInfo="userInfo"
         ></router-view>
       </v-container>
     </v-main>
@@ -18,6 +21,7 @@
 import Sidebar from './Sidebar.vue'
 import Topbar from "./Topbar.vue";
 import notificationApi from "@/api/notification";
+import userApi from "@/api/user";
 
 export default {
   name: "DashboardLayout",
@@ -25,20 +29,27 @@ export default {
     'side-bar': Sidebar,
     'top-bar': Topbar
   },
-  mounted() {
-    this.searchNotificationCounts();
+  async mounted() {
+    await this.searchNotificationCounts();
+    // 사용자 정보 가져오기
+    this.userInfo = await this.getUserInfo();
   },
   data: function() {
     return {
       drawer: true,
-      notificationCounts: 0
+      notificationCounts: 0,
+      userInfo: {},
     }
   },
   methods: {
     searchNotificationCounts: async function() {
       const response = await notificationApi.countsNotification();
       this.notificationCounts = response.data.data;
-    }
+    },
+    getUserInfo: async function() {
+      const response = await userApi.requestUserInfo();
+      return response.data.data;
+    },
   }
 }
 </script>
